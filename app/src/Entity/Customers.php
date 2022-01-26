45f1776b6e1d6f05b83521d662b5532c3c6e6403
stephaned <stephane.dupre@indigen.com>
2022-01-26 18:00:29 +0100

    added new customer entity

---
+++ app/src/Entity/Customers.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+use App\Repository\CustomersRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+use ApiPlatform\Core\Annotation\ApiProperty;
+
+/**
+ * @ApiResource(normalizationContext={"groups"={"outCustomer"}})
+ * @ORM\Entity(repositoryClass=CustomersRepository::class)
+ */
+class Customers
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     * @ApiProperty(identifier=false)
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @ApiProperty(identifier=true)
+     * @Groups({"outCustomer"})
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="boolean")
+     * @Groups({"outCustomer"})
+     */
+    private $isLogged;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     * @Groups({"outCustomer"})
+     */
+    private $address;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getIsLogged(): ?bool
+    {
+        return $this->isLogged;
+    }
+
+    public function setIsLogged(bool $isLogged): self
+    {
+        $this->isLogged = $isLogged;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(?string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+}
